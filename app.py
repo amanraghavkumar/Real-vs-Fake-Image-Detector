1ea0201a8af906ac31ec19e9df6500e72237cd7b
Aman Raghav <amanraghavkumar@gmail.com>
2025-09-22 16:19:38 +0530

    first commit

--- app.py
+++ app.py
@@ -61,4 +61,4 @@ def index():
     return render_template("index.html", result=result, confidence=confidence, img_data=img_data)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=7860)
